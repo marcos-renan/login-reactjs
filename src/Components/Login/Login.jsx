bcb9c2bb18f911de616809aba7f0dbd064a69d27
Marcos <devmarcos7@gmail.com>
2024-11-12 20:39:32 -0300

    instalando tailwind

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,24 +3,24 @@ import {FaUser, FaLock} from "react-icons/fa"
 
 const Login = () => {
   return (
-    <div className="container">
-      <form action="">
-        <h1>Acessar conta</h1>
+    <div >
+      <div className="container">
+        <form action="">
+          <h1>Acessar conta</h1>
 
-        <div className="email">
-          <input type="email" placeholder="E-mail" />
-          <FaUser className="icon" />
+          <div className="email">
+            <input type="email" placeholder="E-mail" />
+            <FaUser className="icon" />
+          </div>
 
-        </div>
+          <div className="senha">
+            <input type="password" placeholder="Senha" />
+            <FaLock className="icon" />
+          </div>
 
-        <div className="senha">
-          <input type="password" placeholder="Senha" />
-          <FaLock className="icon" />
-
-        </div>
-
-        <button>Login</button>
-      </form>
+          <button>Login</button>
+        </form>
+      </div>
     </div>
   );
 };
